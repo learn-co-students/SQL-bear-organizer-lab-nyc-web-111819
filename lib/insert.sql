e13bba593e05f8b577da2953a5181849eb07e703
jasmosez <jas.schaffer@gmail.com>
2019-11-26 16:50:37 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,30 @@
+-- Insert rows into tableO bears'
+INSERT INTO bears
+( -- columns to insert data into
+  name, age, gender, color, temperament, alive
+)
+VALUES
+( -- first row: values for the columns in the list above
+  'Mr. Chocolate', 1, 'Male', 'Brown', 'Nice', 1
+),
+( -- second row: values for the columns in the list above
+  'Rowdy', 2, 'Female', 'Dark Brown', 'Nice', 1
+),
+(
+   'Tabitha', 3, 'Female', 'Dark Brown', 'Nice', 1 
+),
+ (
+   'Sergeant Brown', 4, 'Male', 'Brown', 'Nice', 1 
+),
+ (
+   'Melissa', 5, 'Female', 'Light Brown', 'Not nice', 1 
+),
+ (
+   'Grinch', 6, 'Male', 'Green', 'Not nice', 1
+),
+ (
+   'Wendy', 7, 'Female', 'Light Brown', 'Not nice', 1 
+),
+(
+NULL, 8, 'Female', 'Light Brown', 'Not nice', 1 
+);
